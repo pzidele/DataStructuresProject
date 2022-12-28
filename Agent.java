81d0421b6fab8ab86a7d2b2b29f1869ab2fcbb40
Goldiemax <99050868+Goldiemax@users.noreply.github.com>
2022-12-28 11:01:01 -0500

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -5,7 +5,21 @@ public class Agent {
 	private int agentID;
 	private String fName;
 	private String lName;
-	private int agencyID;
+	private int AgencyID;
 	private double commissionRate;
+	
+	private int agentID;
+	private String fName;
+	private String lName;
+	private int AgencyID;
+	private double commissionRate;
+
+	public Agent(int agentID, String fName, String lName, int AgencyID, double commisionRate){
+		this.agentID = agentID;
+		this.fName = fName;
+		this.lName = lName;
+		this.AgencyID = AgencyID;
+		this.commissionRate = commisionRate;
+	}
 
 }
